bf8c99a1727b47530a495fdf2b7c3b1cbeb32ef2
Anezijane <ahnnieanezi@gmail.com>
2024-04-18 14:07:27 +0100

    add search icon to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-
+import NavBar from './COmponents/NavBar'
 import './index.css'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      GET THE LATEST MOVIE
-    </h1>
+      <NavBar />
+    
     </>
   )
 }
---
+++ src/COmponents/NavBar.jsx
@@ -0,0 +1,23 @@
+import { IoSearch } from "react-icons/io5";
+
+export default function NavBar() {
+    return(
+        <> 
+            <div className="bg-blue-950 text-yellow-300 h-20 pt-4">
+                <nav className="flex text-center justify-between">
+                    <div className="text-2xl">MOVIE~BROWSER</div>
+                    <ul className="flex space-x-3">
+                        <li>LATEST</li>
+                        <li>POPULAR</li>
+                    </ul>
+                    <button type="search" className="bg-white py-1.5 px-3  rounded-lg text-blue-950 flex"><div className="p-1"><IoSearch /></div> Search for movies</button>
+                    <select className="rounded-lg text-blue-950" name="lang" id="eng">
+                        <option value="eng">English</option>
+                        <option value="fn">French</option>
+                    </select>
+                </nav>
+
+            </div>
+        </>
+    )
+}
