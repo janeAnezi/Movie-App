8838686d724a63996ea4da80764698cf653099ff
Anezijane <ahnnieanezi@gmail.com>
2024-04-18 23:56:20 +0100

    add movie list file and content

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import React, { useState } from 'react';
-import NavBar from './COmponents/NavBar'
+import React, { useState } from 'react'
+import NavBar from './Components/NavBar'
+import MovieList from './Components/MovieList'
 import './index.css'
 
 function App() {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([
+    {
+    "Title": "Star Wars: Episode IV - A New Hope",
+    "Year": "1977",
+    "imdbID": "tt0076759",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BOTA5NjhiOTAtZWM0ZC00MWNhLThiMzEtZDFkOTk2OTU1ZDJkXkEyXkFqcGdeQXVyMTA4NDI1NTQx._V1_SX300.jpg"
+    },
+    {
+    "Title": "Star Wars: Episode II - Attack of the Clones",
+    "Year": "2002",
+    "imdbID": "tt0121765",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMDAzM2M0Y2UtZjRmZi00MzVlLTg4MjEtOTE3NzU5ZDVlMTU5XkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SX300.jpg"
+    },
+    {
+    "Title": "Rogue One: A Star Wars Story",
+    "Year": "2016",
+    "imdbID": "tt3748528",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BMjEwMzMxODIzOV5BMl5BanBnXkFtZTgwNzg3OTAzMDI@._V1_SX300.jpg"
+    }
+  ])
 
   return (
     <>
       <NavBar />
-    
+      <MovieList  movies={movies}/>
     </>
   )
 }
