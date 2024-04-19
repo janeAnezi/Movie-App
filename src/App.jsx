2c0e18cd1395acc36b99fadd04d81e2422b5aa93
Anezijane <ahnnieanezi@gmail.com>
2024-04-19 11:05:40 +0100

    add style to scroll through the movie list

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,20 @@ import './index.css'
 function App() {
   const [movies, setMovies] = useState([
     {
+      "Title": "Star Wars: Episode VIII - The Last Jedi",
+      "Year": "2017",
+      "imdbID": "tt2527336",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg"
+    },
+    {
+      "Title": "Star Wars: Episode IX - The Rise of Skywalker",
+      "Year": "2019",
+      "imdbID": "tt2527338",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BMDljNTQ5ODItZmQwMy00M2ExLTljOTQtZTVjNGE2NTg0NGIxXkEyXkFqcGdeQXVyODkzNTgxMDg@._V1_SX300.jpg"
+    },
+    {
     "Title": "Star Wars: Episode IV - A New Hope",
     "Year": "1977",
     "imdbID": "tt0076759",
@@ -30,9 +44,10 @@ function App() {
 
   return (
     <>
-      <div className='bg-indigo-950 text-white'>
+      <div className=' bg-indigo-950 text-white'>
         <NavBar />
         <MovieList  movies={movies}/>
+         
       </div>
       
     </>
