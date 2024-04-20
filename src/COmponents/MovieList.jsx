172b2575fe695ea6d06f0652c615b4ca6204aa13
Anezijane <ahnnieanezi@gmail.com>
2024-04-20 14:09:37 +0100

    remove horizontal scroll bar

--- src/COmponents/MovieList.jsx
+++ src/COmponents/MovieList.jsx
@@ -4,22 +4,34 @@ const MovieList = (props) => {
     const FavouriteComponent = props.favouriteComponent;
 
     return(
-        <>
-            <div className="overflow-x-auto">
-                <div className='min-w-max'>
-                    <div className="flex">
-                        {props.movies.map((movie, index) => (
-                            <div key={index} className="flex-shrink-0 p-5 relative transition-transform hover:cursor-pointer hover:scale-110  ">
+        <> 
+           
+            <div className="overflow-hidden">
+                <div className="flex flex-no-wrap">
+                    {props.movies.map((movie, index) => (
+                        <div key={index} className="flex-shrink-0 p-5 relative transition-transform hover:cursor-pointer hover:scale-110">
                             <img src={movie.Poster} alt="movie" className="w-auto" />
                             <p className="text-center">{movie.Title}</p>
                             <div onClick={() => props.handleFavouritesClick(movie)} className="flex justify-center items-center absolute bg-slate-900 h-20 w-[300px] transition ease-in-out opacity-0 bottom-10 text-sm text-center hover:opacity-90 hover:text-white">
-                                <FavouriteComponent />
+                            <FavouriteComponent />
                             </div>
-                            </div>
-                        ))}
-                    </div>
+                        </div>
+                    ))}
                 </div>
+                <style jsx>{`
+                    .flex-no-wrap {
+                    overflow-x: auto;
+                    scrollbar-width: none;
+                    -ms-overflow-style: none;
+                    }
+                    .flex-no-wrap::-webkit-scrollbar {
+                    display: none; 
+                    }
+                    `}
+                </style>
             </div>
+
+            
         </>
     )
 }
