740c90b511e320faed2ebeae6418ce75c0854030
Anezijane <ahnnieanezi@gmail.com>
2024-04-20 10:50:40 +0100

    add overlay to movies on hover

--- src/COmponents/MovieList.jsx
+++ src/COmponents/MovieList.jsx
@@ -7,9 +7,10 @@ const MovieList = (props) => {
                 <div className='min-w-max'>
                     <div className="flex">
                         {props.movies.map((movie, index) => (
-                            <div key={index} className="flex-shrink-0 p-5">
+                            <div key={index} className="flex-shrink-0 p-5 relative transition-transform hover:cursor-pointer hover:scale-110  ">
                             <img src={movie.Poster} alt="movie" className="w-auto" />
                             <p className="text-center">{movie.Title}</p>
+                            <div className="flex justify-center items-center absolute bg-slate-900 h-full w-80 transition ease-in-out opacity-0 bottom-0 text-2xl p-5 text-center  hover:opacity-30"></div>
                             </div>
                         ))}
                     </div>
